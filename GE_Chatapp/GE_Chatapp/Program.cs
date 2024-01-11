d561ce4550500f7271352a8ba417368557c43580
Snow-EWhitt <ethanwhittaker02@gmail.com>
2024-01-11 13:20:51 -0700

    Removed HTTPS

--- GE_Chatapp/GE_Chatapp/Program.cs
+++ GE_Chatapp/GE_Chatapp/Program.cs
@@ -40,12 +40,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+  Console.WriteLine("In Development");
+
   app.UseWebAssemblyDebugging();
   app.UseSwagger();
   app.UseSwaggerUI();
 }
 else
 {
+  Console.WriteLine("In Production");
+
   app.UseExceptionHandler("/Error", createScopeForErrors: true);
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
   app.UseHsts();
