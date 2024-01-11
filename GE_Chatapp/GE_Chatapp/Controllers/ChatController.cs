9981e0f531f8e46d9530aed3781529533b67686a
garv5014 <garv5014@gmail.com>
2024-01-10 18:17:04 -0700

    added the simple api stuff

--- GE_Chatapp/GE_Chatapp/Controllers/ChatController.cs
+++ GE_Chatapp/GE_Chatapp/Controllers/ChatController.cs
@@ -2,9 +2,32 @@
 
 namespace GE_Chatapp.Controllers;
 
-[Route("api/[controller]")]
+[Route("/api/[controller]")]
 [ApiController]
 public class ChatController : ControllerBase
 {
+  public List<Message> Messages { get; set; } = new List<Message>();
+  public ChatController()
+  {
 
+  }
+
+  [HttpPut]
+  public void Put(Message message)
+  {
+    Messages.Add(message);
+  }
+
+  [HttpGet]
+  public List<Message> Get()
+  {
+    return Messages.OrderBy(m => m.Timestamp).ToList();
+  }
+}
+
+public class Message
+{
+  public string Text { get; set; }
+  public string User { get; set; }
+  public DateTime Timestamp { get; set; }
 }
