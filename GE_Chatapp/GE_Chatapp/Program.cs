55bef3f0e605926f52068be291b2a10fc46af123
garv5014 <garv5014@gmail.com>
2024-01-11 13:32:43 -0700

    removed hsts

--- GE_Chatapp/GE_Chatapp/Program.cs
+++ GE_Chatapp/GE_Chatapp/Program.cs
@@ -40,15 +40,16 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+  Console.WriteLine("Development mode");
   app.UseWebAssemblyDebugging();
   app.UseSwagger();
   app.UseSwaggerUI();
 }
 else
 {
+  Console.WriteLine("Production mode");
   app.UseExceptionHandler("/Error", createScopeForErrors: true);
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
 }
 
 
