88c06bf6e4939995a674891146df48d275c64bb2
garv5014 <garv5014@gmail.com>
2024-01-12 12:08:49 -0700

    is this

--- Chatapp.Shared/Interfaces/IChatService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Chatapp.Shared.Entities;
-
-namespace Chatapp.Shared.Interfaces;
-
-public interface IChatService
-{
-  Task<List<Message>> GetMessagesAsync();
-  Task SendMessageAsync(Message message);
-}
