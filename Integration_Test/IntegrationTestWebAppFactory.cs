e78575013b49dc957c2bc921bbee7f0f39931409
garv5014 <garv5014@gmail.com>
2024-01-15 17:13:36 -0700

    deployment

--- Integration_Test/IntegrationTestWebAppFactory.cs
+++ Integration_Test/IntegrationTestWebAppFactory.cs
@@ -35,7 +35,6 @@ public class IntegrationTestWebAppFactory : WebApplicationFactory<Program>, IAsy
 
   protected override void ConfigureWebHost(IWebHostBuilder builder)
   {
-
     builder.ConfigureTestServices(services =>
     {
       var clientDescriptor = services.FirstOrDefault(d => d.ServiceType == typeof(HttpClient));
