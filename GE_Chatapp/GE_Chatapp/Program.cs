04c93ac497369987a2c173aa9feae81c273491ed
garv5014 <garv5014@gmail.com>
2024-01-18 20:29:54 -0700

    try this now

--- GE_Chatapp/GE_Chatapp/Program.cs
+++ GE_Chatapp/GE_Chatapp/Program.cs
@@ -102,8 +102,6 @@ else
   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 }
 
-
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
