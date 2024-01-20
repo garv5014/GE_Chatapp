bd7b3aaee070cb14ab08732d36ceeac61a704af4
Snow-EWhitt <ethanwhittaker02@gmail.com>
2024-01-20 15:04:58 -0700

    Created 3 metric panels on Grafana

--- Chatapp.Shared/Telemetry/DiagnosticConfig.cs
+++ Chatapp.Shared/Telemetry/DiagnosticConfig.cs
@@ -8,5 +8,8 @@ public static class DiagnosticConfig
   public static string ServiceName = "ge_chatapp";
   public static Meter Meter = new(ServiceName);
   public static Counter<int> userCount = Meter.CreateCounter<int>("users.count");
+  public static Counter<int> messageCount = Meter.CreateCounter<int>("messages.count");
+  public static Counter<int> newMessageFailedCount = Meter.CreateCounter<int>("messages.new.failed.count");
+  public static Counter<int> retrieveAllMessagesFailedCount = Meter.CreateCounter<int>("messages.retrieve.failed.count");
   public static ActivitySource Source = new(ServiceName);
 }
--- GE_Chatapp/GE_Chatapp/Controllers/ChatController.cs
+++ GE_Chatapp/GE_Chatapp/Controllers/ChatController.cs
@@ -12,18 +12,36 @@ namespace GE_Chatapp.Controllers;
 public class ChatController : ControllerBase
 {
   private readonly ChatDbContext _chatDb;
+  // private readonly ILogger _logger;
 
   public ChatController(ChatDbContext chatDb)
   {
     _chatDb = chatDb;
+    // _logger = logger;
   }
 
   [HttpPost]
   public async Task<ActionResult> AddNewMessage([FromBody] Message message)
   {
-    DiagnosticConfig.userCount.Add(1);
-    await _chatDb.Messages.AddAsync(message);
-    await _chatDb.SaveChangesAsync();
+    DiagnosticConfig.messageCount.Add(1);
+    // _logger.LogInformation("Adding message to database");
+    try
+    {
+      if (message.MessageText == "FAILED")
+      {
+        throw new Exception("Message failed");
+      }
+
+      await _chatDb.Messages.AddAsync(message);
+      await _chatDb.SaveChangesAsync();
+    }
+    catch
+    {
+      DiagnosticConfig.newMessageFailedCount.Add(1);
+
+      return StatusCode(500, "Internal server error");
+    }
+
     return Ok();
   }
 
@@ -31,6 +49,16 @@ public class ChatController : ControllerBase
   public async Task<ActionResult<List<Message>>> RetrieveAllMessages()
   {
     var message = await _chatDb.Messages.ToListAsync();
+
+    // _logger.LogInformation("Retrieving all messages");
+
+    if (message == null)
+    {
+      DiagnosticConfig.retrieveAllMessagesFailedCount.Add(1);
+
+      return StatusCode(500, "Messages are null");
+    }
+
     return message.OrderBy(m => m.CreatedAt).ToList();
   }
 }
