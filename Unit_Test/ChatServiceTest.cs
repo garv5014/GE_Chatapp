a27c8fd821ea9e0a65b2e65a40b2ee38723deda0
garv5014 <garv5014@gmail.com>
2024-01-31 16:22:35 -0700

    fixed unit test

--- Unit_Test/ChatServiceTest.cs
+++ Unit_Test/ChatServiceTest.cs
@@ -1,65 +1,46 @@
-﻿using System.Net;
-using System.Text.Json;
-
-using Chatapp.Shared.Entities;
+﻿using Chatapp.Shared.Entities;
 using Chatapp.Shared.Interfaces;
-using Chatapp.Shared.Services;
 using Chatapp.Shared.Simple_Models;
 
 using Moq;
-using Moq.Protected;
 
 namespace Chatapp.Tests
 {
   public class ChatServiceTests
   {
-    private Mock<HttpMessageHandler>? _mockHttpMessageHandler;
-    private HttpClient? _mockHttpClient;
-    private ChatService? _chatService;
 
     [SetUp]
     public void Setup()
     {
-      _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
-      _mockHttpMessageHandler.Protected()
-          .Setup<Task<HttpResponseMessage>>(
-              "SendAsync",
-              ItExpr.IsAny<HttpRequestMessage>(),
-              ItExpr.IsAny<CancellationToken>())
-          .ReturnsAsync(new HttpResponseMessage
-          {
-            StatusCode = HttpStatusCode.OK,
-            Content = new StringContent(JsonSerializer.Serialize(new List<MessageWithImages> { new MessageWithImages { Message = new Message { MessageText = "Test Message" } } }))
-          });
 
-      _mockHttpClient = new HttpClient(_mockHttpMessageHandler.Object)
-      {
-        BaseAddress = new Uri("http://test.com")
-      };
-      var mockFileService = new Mock<IFileAPIService>();
-
-      mockFileService.Setup(m => m.RetrieveImageFromFileApi(It.IsAny<string>())).ReturnsAsync("");
-
-      _chatService = new ChatService(_mockHttpClient, mockFileService.Object);
     }
 
     [Test]
     public async Task GetMessagesAsync_ReturnsListOfMessages()
     {
-      var result = await _chatService.GetMessagesAsync();
+      var chatMock = new Mock<IChatService>();
+
+      chatMock.Setup(x => x.GetMessagesAsync()).ReturnsAsync(new List<MessageWithImages>
+      {
+        new MessageWithImages
+        {
+          Message = new Message
+          {
+            Id = 1,
+            MessageText = "Test Message",
+            Username = "Test User",
+            CreatedAt = DateTime.Now
+          },
+          Images = new List<string>()
+        }
+      });
+
+      var result = await chatMock.Object.GetMessagesAsync();
 
       Assert.IsNotNull(result);
       Assert.That(result, Has.Count.EqualTo(1));
+      Console.WriteLine($"here is the result {result[0].Message.Id}");
       Assert.That(result[0].Message.MessageText, Is.EqualTo("Test Message"));
-
-      // Verify that the GetAsync method was called only once
-      _mockHttpMessageHandler.Protected().Verify(
-          "SendAsync",
-          Times.Once(),
-          ItExpr.Is<HttpRequestMessage>(req =>
-              req.Method == HttpMethod.Get
-              && req.RequestUri.ToString().EndsWith("api/chat")),
-          ItExpr.IsAny<CancellationToken>());
     }
   }
 }
