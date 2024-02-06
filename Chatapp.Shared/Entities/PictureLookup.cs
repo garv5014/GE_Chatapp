14fe69daace5c0a124b79f1aa4d192544ff32203
garv5014 <garv5014@gmail.com>
2024-02-05 19:05:04 -0700

    lookup entity

---
+++ Chatapp.Shared/Entities/PictureLookup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chatapp.Shared.Entities;
+
+public partial class PictureLookup
+{
+  public int Id { get; set; }
+
+  public int PictureId { get; set; }
+
+  public string MachineName { get; set; } = null!;
+
+  public virtual Picture Picture { get; set; } = null!;
+}
