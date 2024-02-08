6ff4477f198d9e88c716b50f78f74a4cd3a23f11
garv5014 <garv5014@gmail.com>
2024-02-08 14:02:17 -0700

    Docker build worked

--- RedundancyService/Program.cs
+++ RedundancyService/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string listOfFileServiceURLs = Environment.GetEnvironmentVariable("FILE_SERIVCE_NAMES");
+
+int sleepInterval = int.Parse(Environment.GetEnvironmentVariable("SLEEP_INTERVAL"));
+if (listOfFileServiceURLs == null)
+{
+  throw new Exception("Requires a list of serviceURLS");
+}
+
+string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
+
+//make dbcontext
+
+List<string> listOfServices = new List<string>();
+listOfServices = listOfFileServiceURLs.Split(',').ToList();
+
+while (true)
+{
+  Task.Delay(sleepInterval * 1000).Wait();
+  //get list of non-replicated pictures
+  // for each picture send to and image api /replicate endpoint that is not its owner for replication
+  Console.WriteLine("Hello, World!");
+}
