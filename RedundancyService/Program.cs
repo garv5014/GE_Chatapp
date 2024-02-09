e18dca77b5567654f29c548abe02cc59bb8b9106
garv5014 <garv5014@gmail.com>
2024-02-08 18:41:39 -0700

    Added redundancy service database lookup

--- RedundancyService/Program.cs
+++ RedundancyService/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Chatapp.Shared;
+using Chatapp.Shared.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
 string listOfFileServiceURLs = Environment.GetEnvironmentVariable("FILE_SERIVCE_NAMES");
 
 int sleepInterval = int.Parse(Environment.GetEnvironmentVariable("SLEEP_INTERVAL"));
@@ -9,8 +14,12 @@ if (listOfFileServiceURLs == null)
 
 string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
 
-//make dbcontext
+var optionsBuilder = new DbContextOptionsBuilder<ChatDbContext>();
+optionsBuilder.UseNpgsql(connectionString);
+optionsBuilder.EnableDetailedErrors();
+optionsBuilder.EnableSensitiveDataLogging();
 
+var db = new ChatDbContext(options: optionsBuilder.Options);
 List<string> listOfServices = new List<string>();
 listOfServices = listOfFileServiceURLs.Split(',').ToList();
 
@@ -18,6 +27,25 @@ while (true)
 {
   Task.Delay(sleepInterval * 1000).Wait();
   //get list of non-replicated pictures
+  List<PictureLookup> uniquePictures = await db.PictureLookups
+    .GroupBy(e => e.PictureId)
+    .Where(g => g.Count() == 1)
+    .Select(g => g.First())
+    .ToListAsync();
+
   // for each picture send to and image api /replicate endpoint that is not its owner for replication
+  foreach (var image in uniquePictures)
+  {
+    Random random = new Random();
+
+    var filteredList = listOfServices.Where(e => e != image.MachineName).ToList();
+    int randomServiceIndex = random.Next(0, filteredList.Count);
+
+    HttpClient client = new HttpClient();
+    client.BaseAddress = new Uri($"http://{filteredList[randomServiceIndex]}:8080");
+    await client.PostAsync($"/replicate/{image.PictureId}", null);
+
+  }
+
   Console.WriteLine("Hello, World!");
 }
