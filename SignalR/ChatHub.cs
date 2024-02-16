591e941b687cb267a023fc4f9e81f000cad840ae
garv5014 <garv5014@gmail.com>
2024-02-15 20:31:08 -0700

    signalR Working

--- SignalR/ChatHub.cs
+++ SignalR/ChatHub.cs
@@ -6,6 +6,20 @@ public class ChatHub : Hub
 {
   public async Task SendMessage(string user, string message)
   {
-    await Clients.All.SendAsync("ReceiveMessage", user, message);
+    await Clients.Others.SendAsync("ReceiveMessage", user, message);
+  }
+
+  public async Task StartTyping(string user)
+  {
+    Console.WriteLine("Start Typing");
+
+    await Clients.Others.SendAsync("ReceiveStartTyping", user);
+  }
+
+  public async Task StopTyping(string user)
+  {
+    Console.WriteLine("Stop Typing");
+
+    await Clients.Others.SendAsync("ReceiveStopTyping", user);
   }
 }
